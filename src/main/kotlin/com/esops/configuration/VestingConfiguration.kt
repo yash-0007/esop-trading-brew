12fbcef9c40238a7504fe93ae5af27d4fbfde202
Yash Nale <yashn@sahaj.ai>
2023-02-08 17:09:58 +0530

    Move configuration variables to constructor

--- src/main/kotlin/com/esops/configuration/VestingConfiguration.kt
+++ src/main/kotlin/com/esops/configuration/VestingConfiguration.kt
@@ -2,13 +2,8 @@ package  com.esops.configuration
 
 import com.esops.validation.configuration.BreakupArrayValid
 import io.micronaut.context.annotation.ConfigurationProperties
-import java.math.BigInteger
 import javax.validation.constraints.Min
 
 @ConfigurationProperties("vesting")
-class VestingConfiguration {
-    @BreakupArrayValid
-    var breakup: Array<Double> = arrayOf(0.3, 0.2, 0.1, 0.4)
-    @Min(0)
-    var duration: Int = 0
+class VestingConfiguration(@BreakupArrayValid var breakup: Array<Double> = arrayOf(0.3, 0.2, 0.1, 0.4), @field:Min(0) var duration: Int = 0) {
 }
