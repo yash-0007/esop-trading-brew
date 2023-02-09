04c8acec9e1d15c941de0cd8aaf27f73be3a8ad2
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-02-09 11:38:37 +0530

    Change return type from Any to Order in OrderController

--- src/main/kotlin/com/esops/controller/OrderController.kt
+++ src/main/kotlin/com/esops/controller/OrderController.kt
@@ -2,9 +2,7 @@ package com.esops.controller
 
 import com.esops.entity.Order
 import com.esops.model.AddOrderRequestBody
-import com.esops.model.ErrorResponse
 import com.esops.service.OrderService
-import com.esops.service.UserService
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
@@ -14,21 +12,14 @@ import javax.validation.Valid
 @Validated
 @Controller
 class OrderController(
-    private var userService: UserService,
     private var orderService: OrderService
 ) {
 
     @Post(uri = "/user/{userName}/order")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    fun order(@Body @Valid addOrderRequestBody: AddOrderRequestBody, userName: String): HttpResponse<Any> {
-        val error = this.userService.canAddOrder(userName, addOrderRequestBody)
-        return if (error.isNotEmpty()) {
-            HttpResponse.badRequest(ErrorResponse(error))
-        } else {
-            HttpResponse.ok(this.orderService.placeOrder(userName, addOrderRequestBody))
-        }
-    }
+    fun order(@Body @Valid addOrderRequestBody: AddOrderRequestBody, userName: String): HttpResponse<Order> =
+        HttpResponse.ok(this.orderService.placeOrder(userName, addOrderRequestBody))
 
     @Get(uri = "/user/{userName}/order")
     @Produces(MediaType.APPLICATION_JSON)
