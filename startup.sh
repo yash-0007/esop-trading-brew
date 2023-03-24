1b7b6ef22d9ec5ff89a8bd8e8a19910197e1b597
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-24 16:46:09 +0530

    Add command to install java

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo lsof -t -i:80 | sudo xargs kill -9
+#sudo kill -9 $(lsof -t -i:80)
 
 export MICRONAUT_SERVER_PORT=80
+
+sudo yum install java-17-amazon-corretto-headless -y > /dev/null
 
 nohup sudo java -jar ~/esop-trading-application/esop-trading-0.1-all.jar > /dev/null &
