123c7bcdfa88b0764e342a67cea8f66db948ca8b
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-24 17:07:40 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#sudo kill -9 $(lsof -t -i:80)
+sudo lsof -t -i:80 | sudo xargs kill -9
 
-export MICRONAUT_SERVER_PORT=80
+sudo yum install java-17-amazon-corretto-headless -y
 
-sudo yum install java-17-amazon-corretto-headless -y > /dev/null
-
-nohup sudo java -jar ~/esop-trading-application/esop-trading-0.1-all.jar > /dev/null &
+nohup sudo MICRONAUT_SERVER_PORT=80 java -jar ~/esop-trading-application/esop-trading-0.1-all.jar /dev/null 2>&1 &
