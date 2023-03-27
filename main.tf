5ed0d52cb7f42fc227c02644654ca424de040431
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-27 12:22:47 +0530

    Add terraform script for infra

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "vinayak-gurukul"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_instance" "esoptrading" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  associate_public_ip_address = var.associate_public_ip_address
+  key_name = var.key_name
+  
+  tags = var.tags
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = var.key
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "yum install java-17-amazon-corretto-headless -y",
+    ]
+  }
+
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "ami_id" {
+  default = "ami-00c39f71452c08778"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "associate_public_ip_address" {
+  default = true
+}
+
+variable "tags" {
+  default = {
+    Name = "gurukul_esoptrading_vinayak"
+  }
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  default = "key-vinayak"
+}
+
+variable "key" {
+  type = string
+  default = ""
+}
+
+variable "tf_state_bucket" {
+  default = "gurukul-vinayak"
+}
+
+variable "tf_state_key" {
+  default = "terraform.tfstate"
+}
