c7245232077b769ee530b7313db11ddc3c5f48a3
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-27 14:56:17 +0530

    Remove java installation from bash script

--- startup.sh
+++ startup.sh
@@ -2,6 +2,4 @@
 
 sudo lsof -t -i:80 | sudo xargs kill -9
 
-sudo yum install java-17-amazon-corretto-headless -y
-
 nohup sudo MICRONAUT_SERVER_PORT=80 java -jar ~/esop-trading-application/esop-trading-0.1-all.jar /dev/null 2>&1 &
