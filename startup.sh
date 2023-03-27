99f8282937861fc8adf613a7beb4507aae851249
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-27 15:20:49 +0530

    Fix bug in output redirection

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 
 sudo lsof -t -i:80 | sudo xargs kill -9
 
-nohup sudo MICRONAUT_SERVER_PORT=80 java -jar ~/esop-trading-application/esop-trading-0.1-all.jar /dev/null 2>&1 &
+nohup sudo MICRONAUT_SERVER_PORT=80 java -jar ./build/libs/esop-trading-0.1-all.jar > /dev/null &
